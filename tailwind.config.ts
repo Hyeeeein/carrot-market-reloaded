0a00e0f64faa887c1348cd6e50223b0064d7585a
hyein <khi_9866@naver.com>
2025-02-21 23:36:46 +0900

    feat: 3, 4 챕터 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 export default {
   content: [
+    // 아래 파일들 클래스네임에 입력된 값을 컴파일
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      // 커스텀
       margin: {
         tomato: "120px",
       },
@@ -16,5 +19,7 @@ export default {
       },
     },
   },
-  plugins: [],
+
+  // plugins: base, components, utilities layer 를 확장
+  plugins: [require("@tailwindcss/forms")],
 } satisfies Config;
